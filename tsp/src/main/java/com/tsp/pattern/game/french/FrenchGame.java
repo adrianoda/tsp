667283ddfc5af91fd10af2e7b50b4a9f28562c64
daniele.adriano <daniele.adriano@mitobit.com>
2017-10-25 18:06:12 +0200

    added comments

--- tsp/src/main/java/com/tsp/pattern/game/french/FrenchGame.java
+++ tsp/src/main/java/com/tsp/pattern/game/french/FrenchGame.java
@@ -11,27 +11,33 @@ public class FrenchGame {
 	public FrenchGame(FrenchPlayer p1, FrenchPlayer p2) {
 		this.p1 = p1;
 		this.p2 = p2;
+
+		// Create a new deck
 		FrenchDeckBuilder db = new FrenchDeckBuilder();
 		deck = db.createDeck();
 		System.out.println(deck);
+
+		// Distribute 3 cards for each player
 		distribute(deck, p1);
 		distribute(deck, p2);
+
+		// Then set up the briscol
 		briscolCard = deck.getCard();
 		briscolSuit = briscolCard.getSuit();
+		System.out.println("Briscol: " + briscolCard.toString());
 	}
 
 	public void play() {
-		System.out.println("Briscol: " + briscolCard.toString());
-
 		FrenchPlayer winner = p1;
 		FrenchPlayer looser = p2;
 		FrenchPlayer tmp;
 		while (!deck.isEmpty() || !p1.isHandEmpty() || !p1.isHandEmpty()) {
+			// Each player play a card
 			FrenchCard c1 = winner.drop();
 			FrenchCard c2 = looser.drop();
 			System.out.println("winner " + winner + " play " + c1 + " - looser " + looser + " play " + c2);
 
-			// Switch state
+			// Choose the winner and switch state
 			int win = drop(c1, c2);
 			if (win == -1) {
 				tmp = winner;
@@ -39,21 +45,26 @@ public class FrenchGame {
 				looser = tmp;
 			}
 			System.out.println("wins " + winner);
+
+			// Add cards to winner score dsk
 			winner.addToScore(c1).addToScore(c2);
+
+			// Again, one card for each player
 			distribute(deck, winner);
 			distribute(deck, looser);
 		}
-
 		System.out.println(deck.toString());
 		System.out.println("p1 deck size: " + p1.getScore().size() + ", total score: " + p1.getTotalScore());
 		System.out.println("p2 deck size: " + p2.getScore().size() + ", total score: " + p2.getTotalScore());
 	}
 
 	private void distribute(FrenchDeck deck, FrenchPlayer player) {
+		// Last card to be picked up is the briscola card
 		if (deck.isEmpty() && briscolCard != null) {
 			deck.addCard(briscolCard);
 			briscolCard = null;
 		}
+		// Fill player hand
 		while (!deck.isEmpty() && !player.isHandFull()) {
 			player.addToHand(deck.getCard());
 		}
@@ -66,6 +77,7 @@ public class FrenchGame {
 			return 1;
 		}
 		if (c1.getSuit().toString().equals(c2.getSuit().toString())) {
+			// same suit, the higher value the winner!
 			return c1.getValue().getValue().compareTo(c2.getValue().getValue());
 		} else if (c1.getSuit().toString().equals(briscolSuit.toString())) {
 			// c1 is a briscol
